64c2c51d4608fb8ab76bede8546608db43714c70
floydian86 <161500978+floydian86@users.noreply.github.com>
2025-08-22 11:35:36 -0500

    Create doc_ai_objects.sql

---
+++ doc_ai_objects.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE doc_ai_db;
+CREATE SCHEMA doc_ai_db.doc_ai_schema;
+
+USE ROLE ACCOUNTADMIN;
+
+CREATE ROLE doc_ai_role;
+
+
+GRANT DATABASE ROLE SNOWFLAKE.DOCUMENT_INTELLIGENCE_CREATOR TO ROLE doc_ai_role;
+
+GRANT USAGE, OPERATE ON WAREHOUSE DOC_AI_WH TO ROLE doc_ai_role;
+
+GRANT USAGE ON DATABASE doc_ai_db TO ROLE doc_ai_role;
+GRANT USAGE ON SCHEMA doc_ai_db.doc_ai_schema TO ROLE doc_ai_role;
+
+GRANT CREATE STAGE ON SCHEMA doc_ai_db.doc_ai_schema TO ROLE doc_ai_role;
+
+GRANT CREATE SNOWFLAKE.ML.DOCUMENT_INTELLIGENCE ON SCHEMA doc_ai_db.doc_ai_schema TO ROLE doc_ai_role;
+GRANT CREATE MODEL ON SCHEMA doc_ai_db.doc_ai_schema TO ROLE doc_ai_role;
+
+
+GRANT CREATE STREAM, CREATE TABLE, CREATE TASK, CREATE VIEW ON SCHEMA doc_ai_db.doc_ai_schema TO ROLE doc_ai_role;
+GRANT EXECUTE TASK ON ACCOUNT TO ROLE doc_ai_role;
+
+
+GRANT ROLE doc_ai_role TO USER ganeshiyer;
